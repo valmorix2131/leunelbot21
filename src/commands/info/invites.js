7636dac41f15f276c1f779032ad51bbf3267729c
valmorix2131 <71914065+valmorix2131@users.noreply.github.com>
2021-03-29 15:41:15 +0800

    Add files via upload

---
+++ src/commands/info/invites.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const Command = require('../Command.js');
+
+module.exports = class InviteCommand extends Command {
+    constructor(client) {
+      super(client, {
+        name: "invitations",
+        aliases: ['invites'],
+        category: "info",
+        description: "Shows Users Joined Through Someone's Invites",
+        usage: "[name | nickname | mention | ID] (optional)",
+        type: client.types.INFO
+      });
+    }
+    async run(message, args) {
+        try {
+            let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+
+            let invites = await message.guild.fetchInvites()
+
+            let memberInvites = invites.filter(i => i.inviter && i.inviter.id === member.user.id);
+
+            if (memberInvites.size <= 0) {
+                return message.channel.send(`**${member.displayName} didn't invite anyone to the server!**`, (member === message.member ? null : member));
+  {}          }
+
+            let content = memberInvites.map(i => i.code).join("\n");
+            let index = 0;
+            memberInvites.forEach(invite => index += invite.uses);
+
+            let embed = new Discord.MessageEmbed()
+                .setColor("GREEN")
+                .setFooter(message.guild.name, message.guild.iconURL())
+                .setAuthor(`Invite Tracker for ${message.guild.name}`)
+                .setDescription(`Information on Invites of ${member.displayName}`)
+                .addField("**No. Invited Persons**", index)
+            message.channel.send(embed);
+        } catch (e) {
+            return message.channel.send(e.message)
+        }
+    }
+};
